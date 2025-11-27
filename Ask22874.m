dd651c76e7a63bf06946e438c8052797a19950e3
Chondromatidis Eleftherios <148573871+ChondromatidisEleftherios@users.noreply.github.com>
2025-11-27 18:54:51 +0200

    Update 27/11/2025 (Added more readable messages)

--- Ask22874.m
+++ Ask22874.m
@@ -1,6 +1,6 @@
 function Ask22874()
 A = [12, -7, 9, 5; 8,15, -6, 11; 14,9,17,-8; 10, -4, 5, 13];
-disp("Starting Matrix A:");
+disp("Starting Matrix A: ");
 disp(A);
 
 disp("---Calculating the RREF of A---");
